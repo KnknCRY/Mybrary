a2747884ae2db718117f2cb85a5b608933f61f37
RueiYang <s35016080@gmail.com>
2020-04-17 10:46:05 +0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,25 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").parse();
+// }
+
+const express = require("express");
+const app = express();
+const expressLayout = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index.js");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.set("layout", "layouts/layout");
+app.use(expressLayout);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mybrary", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.log("error"));
+db.once("open", () => console.log("connected to mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
